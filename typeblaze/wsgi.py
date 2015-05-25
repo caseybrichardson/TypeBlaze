18002829482ec8cd560f3acee63ba3f2c28917b7
Casey Richardson <casey@caseybrichardson.com>
2015-05-24 21:51:21 -0500

    Initial Commit

---
+++ typeblaze/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for typeblaze project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeblaze.settings")
+
+application = get_wsgi_application()
